8b9336372a4f314c0794785efedf3b093fb38daa
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-30 18:06:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,25 @@
 # Code your solution here!
+def prompt_user
+  
+end
+
+def generate_number
+  rand(1..6)
+end
+
+def get_user_input
+  gets
+end
+
+def is_winner? (guess, correct_number)
+  guess == correct_number
+end
+
+
+def run_guessing_game
+  prompt_user
+  guess = get_user_input
+  correct_number = generate_number
+  if is_winner? (guess, correct_number)
+    puts ""
+end
